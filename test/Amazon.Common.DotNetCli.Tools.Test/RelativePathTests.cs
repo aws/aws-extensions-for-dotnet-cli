22b90de930c2f12fb58117b426d054990e2b82b3
Norm Johanson <normj@amazon.com>
2018-07-09 22:43:44 -0700

    Make test compatible for non-windows platform

--- test/Amazon.Common.DotNetCli.Tools.Test/RelativePathTests.cs
+++ test/Amazon.Common.DotNetCli.Tools.Test/RelativePathTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 
 using Xunit;
@@ -9,16 +10,19 @@ namespace Amazon.Common.DotNetCli.Tools.Test
     public class RelativePathTests
     {
         [Theory]
-        [InlineData(@"C:\Code\Solution\Project", @"C:\Code\Solution", "../")]
-        [InlineData(@"c:\code\solution\project", @"C:\Code\Solution", "../")]
-        [InlineData(@"C:\Code\Solution\ProjectA", @"C:\Code\Solution\ProjectB\file.csproj", "../ProjectB/file.csproj")]
-        [InlineData(@"C:\Code\Solution\Project", @"D:\Code\Solution", "D:/Code/Solution")]
-        [InlineData(@"C:\Code\Solution", @"C:\Code\Solution\Project", "Project")]
-        [InlineData(@"C:\Code\Solution", @"C:\Code\Solution\Project\foo.cs", "Project/foo.cs")]
-        [InlineData(@"/home/user/Solution/Project", @"/home/user/Solution", "../")]
-        [InlineData(@"/home/user/Solution", @"/home/user/Solution/Project", "Project")]
-        public void ToParentDirectory(string start, string relativeTo, string expected)
+        [InlineData(@"C:\Code\Solution\Project", @"C:\Code\Solution", "../", false)]
+        [InlineData(@"c:\code\solution\project", @"C:\Code\Solution", "../", false)]
+        [InlineData(@"C:\Code\Solution\ProjectA", @"C:\Code\Solution\ProjectB\file.csproj", "../ProjectB/file.csproj", false)]
+        [InlineData(@"C:\Code\Solution\Project", @"D:\Code\Solution", "D:/Code/Solution", true)]
+        [InlineData(@"C:\Code\Solution", @"C:\Code\Solution\Project", "Project", false)]
+        [InlineData(@"C:\Code\Solution", @"C:\Code\Solution\Project\foo.cs", "Project/foo.cs", false)]
+        [InlineData(@"/home/user/Solution/Project", @"/home/user/Solution", "../", false)]
+        [InlineData(@"/home/user/Solution", @"/home/user/Solution/Project", "Project", false)]
+        public void ToParentDirectory(string start, string relativeTo, string expected, bool windowsOnly)
         {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && windowsOnly)
+                return;
+            
             Assert.Equal(expected, Utilities.RelativePathTo(start, relativeTo));
         }
     }
