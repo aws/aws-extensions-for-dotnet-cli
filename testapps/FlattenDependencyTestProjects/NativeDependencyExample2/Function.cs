a81779eaeff837819aed6d74e8141a1b58df7ced
Denys Zhuravel <denys.zhuravel@albelli.com>
2018-08-20 15:26:44 +0200

    Add an integrational test to verify the package contents

---
+++ testapps/FlattenDependencyTestProjects/NativeDependencyExample2/Function.cs
@@ -0,0 +1,22 @@
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace NativeDependencyExample2
+{
+    public class Function
+    {
+        
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public string FunctionHandler(string input, ILambdaContext context)
+        {
+            return input?.ToUpper();
+        }
+    }
+}
