ae56c7264f062a3d8c44f010cb79b3ba47a12724
Norm Johanson <normj@amazon.com>
2021-11-13 20:34:15 -0800

    Fix compiler warning

--- src/Amazon.Lambda.Tools/LambdaPackager.cs
+++ src/Amazon.Lambda.Tools/LambdaPackager.cs
@@ -20,10 +20,15 @@ namespace Amazon.Lambda.Tools
         private const string Shebang = "#!";
         private const char LinuxLineEnding = '\n';
         private const string BootstrapFilename = "bootstrap";
+#if NETCOREAPP3_1_OR_GREATER        
+        private static readonly string BuildLambdaZipCliPath = Path.Combine(
+            Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().Location).LocalPath),
+            "Resources\\build-lambda-zip.exe");
+#else
         private static readonly string BuildLambdaZipCliPath = Path.Combine(
             Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath),
             "Resources\\build-lambda-zip.exe");
-
+#endif
         static IDictionary<string, Version> NETSTANDARD_LIBRARY_VERSIONS = new Dictionary<string, Version>
         {
             { "netcoreapp1.0", Version.Parse("1.6.0") },
--- testapps/TestLayerAspNetCore/Startup.cs
+++ testapps/TestLayerAspNetCore/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace TestLayerAspNetCore
@@ -32,7 +33,7 @@ namespace TestLayerAspNetCore
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
