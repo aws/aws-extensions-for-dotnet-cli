5c8d1dc9d69355c2cba486ae02ac7c2c14c100a8
Ashish Dhingra <67916761+ashishdhingra@users.noreply.github.com>
2024-10-30 14:33:04 -0700

    Fixed an issue where primitive values in payload for InvokeFunctionCommand were not working.

---
+++ testapps/TestIntegerFunction/Function.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestIntegerFunction
+{
+    
+    public class Function
+    {
+        [Amazon.Lambda.Core.LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+        public string FunctionHandler(int input)
+        {
+            return "Hello " + input;
+        }
+    }
+}
